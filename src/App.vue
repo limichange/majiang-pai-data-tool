a75c9ba3271aaf55dc0e3c8a3984bd753419f828
limichange <limichange@hotmail.com>
2017-07-20 17:20:17 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  width: 960px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/indexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="indexPage">
+    <el-input></el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'indexPage',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
